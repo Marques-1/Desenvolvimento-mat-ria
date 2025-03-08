41ab41651b7669bded5fefe98695a1f0c6beac5a
D. <143917828+Marques-1@users.noreply.github.com>
2025-03-08 15:54:32 +0000

    salvar

---
+++ projeto/Condicionais.java
@@ -0,0 +1,24 @@
+public class Condicionais {
+public static void main(String[] args) {
+    
+int a = 10;
+int b = 2;
+int maior;
+
+if(a > b){
+
+maior = a;
+
+} else {
+
+maior = b;   
+    
+}
+
+System.out.println("O maior número é:" + maior);
+
+}
+
+
+
+}
---
+++ projeto/Variaveis.java
@@ -0,0 +1,17 @@
+public class Variaveis {
+public static void main(String[] args) {
+    String nome = "Marta";
+    int idade = 23;
+    double peso = 60.200;
+
+ //Constante   
+final String CPF = "012.345.678.90";
+
+System.out.println("Nome: " + nome);
+System.out.println("idade: " + idade);
+System.out.println("Peso: " + peso);
+System.out.println("Cpf:" + CPF );
+
+}
+
+}
